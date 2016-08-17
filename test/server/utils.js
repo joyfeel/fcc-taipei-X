f4c9146fff7f45d03837d0bccd35d6712d14f71f
joyfeel <joybee210@gmail.com>
2016-08-17 17:35:47 +0800

    Finished: signup spec

--- test/server/utils.js
+++ test/server/utils.js
@@ -2,5 +2,5 @@ import supertest from 'supertest'
 import app from '../../server'
 
 export default function request() {
-    return supertest.agent(app.listen())
+  return supertest.agent(app.listen())
 }
