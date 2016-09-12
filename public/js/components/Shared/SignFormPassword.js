e294386aea9ca0a7b53b125ebe781a967980bddf
Doma Lu <kaku18ccf52@gmail.com>
2016-09-12 10:56:32 +0800

    Click eye icon of password input can switch type to text or email

--- public/js/components/Shared/SignFormPassword.js
+++ public/js/components/Shared/SignFormPassword.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import cx from 'classnames'
 
-const SignFormPassword = () =>
-  <div className='sign-form-password'>
-    <input type="password" name="password"  id="password" placeholder='password' className="password-input"/>
-    <label className='password-icon' htmlFor="password"></label>
-    <i className='eye-closed'></i>
-  </div>
+class SignFormPassword extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { eyeToggle : false }
+    this.visible = this.visible.bind(this)
+  }
+  visible() {
+    this.setState({ eyeToggle : !this.state.eyeToggle })
+  }
+  render() {
+    const { eyeToggle } = this.state
+    const eyeClasses = cx({
+      'eye-opened': eyeToggle,
+      'eye-closed': !eyeToggle
+    })
+
+    return (
+      <div className='sign-form-password'>
+        <input type={eyeToggle ? 'text' : 'password'} name='password' id='password' placeholder='password' className='password-input' />
+        <label className='password-icon' htmlFor='password'></label>
+        <i className={eyeClasses} onClick={this.visible}></i>
+      </div>
+    )
+  }
+}
 
 export default SignFormPassword
