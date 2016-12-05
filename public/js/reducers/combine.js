6100503f5c6daa290fa72cf76766e338979675ff
Doma Lu <kaku18ccf52@gmail.com>
2016-12-05 10:50:34 +0800

    Add findOlderFetching state in combine redux flow

--- public/js/reducers/combine.js
+++ public/js/reducers/combine.js
@@ -2,20 +2,35 @@ import * as ActionTypes from '../actions/combine'
 
 const initialState = {
   isFetching: false,
+  findOlderFetching: false,
 }
 
 const combine = (state = initialState, action) => {
   switch (action.type) {
     case ActionTypes.SENDING_REQUEST:
-      return {
-        ...state,
-        isFetching: true,
+      if(action.fetch === undefined) {
+        return {
+          ...state,
+          isFetching: true,
+        }
+      } else {
+        return {
+          ...state,
+          ...action.fetch,
+        }
       }
     case ActionTypes.CANCEL_REQUEST:
-      return {
-        ...state,
-        isFetching: false,
-      }
+        if(action.fetch === undefined) {
+          return {
+            ...state,
+            isFetching: false,
+          }
+        } else {
+          return {
+            ...state,
+            ...action.fetch,
+          }
+        }
     default:
       return state
   }
